c7fe0d76b6da3ba242b99d79788dfb0cae80e694
indoorsglobal <indoorsglobal123@gmail.com>
2026-04-20 11:49:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function page() {
       <Category />
       <About />
       <Features />
-      {/* <PortraitSlider/> */}
+       <PortraitSlider/> 
       <TestimonialSection/>
       <Blog />
       <CTA />
