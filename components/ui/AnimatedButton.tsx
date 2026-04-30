b44ff48a1bcf8f34d1d9929b6b2f8683555b3cd5
indoorsglobal <indoorsglobal123@gmail.com>
2026-04-30 11:16:17 +0530

    Update AnimatedButton.tsx

--- components/ui/AnimatedButton.tsx
+++ components/ui/AnimatedButton.tsx
@@ -5,13 +5,13 @@ const AnimatedButton = () => {
   return (
     <NextLink
       href="/blog"
-      className="group relative hidden sm:flex items-center gap-2 px-3 md:px-5 py-2 text-[13px] font-semibold uppercase tracking-wider text-[#009341] transition-all rounded-md overflow-hidden"
+      className="group relative sm:flex items-center gap-2 px-3 md:px-5 py-2 text-[13px] max-lg:text-gray-800 max-lg:text-[16px] font-semibold uppercase tracking-wider text-[#009341] transition-all rounded-md overflow-hidden"
     >
       {/* SVG Border Layer 
           We use vector-effect="non-scaling-stroke" to keep the border width consistent 
       */}
       <svg
-        className="absolute inset-0 h-full w-full pointer-events-none"
+        className="absolute inset-0 h-full  w-full pointer-events-none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <rect
@@ -32,9 +32,11 @@ const AnimatedButton = () => {
       </svg>
 
       {/* Content Layer */}
-      <div className="relative z-10 flex items-center gap-2">
-        <GrWorkshop size={18} />
-        <span className="hidden xl:inline">Workshop</span>
+      <div className="relative z-10  flex items-center gap-2">
+        <div className="max-md:hidden">
+          <GrWorkshop size={18} />
+        </div>
+        <span className="xl:inline ">Workshop</span>
       </div>
 
       {/* Optional: Subtle background fade on hover */}
